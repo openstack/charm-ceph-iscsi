bf30af291efddee483825bd986c65242984793df
Samuel Walladge <samuel.walladge@canonical.com>
2023-10-26 10:19:02 +1030

    Fix charm for tox4 compatibility
    Also remove python 3.5 targets as this is long not supported.
    
    And add the mocking for charmhelpers - see
    https://review.opendev.org/c/openstack/charm-heat/+/899195 for example
    
    Related-Bug: 2002788
    Change-Id: Ic70092a45d595d337ea26505139c2f6515cb1d38

--- unit_tests/__init__.py
+++ unit_tests/__init__.py
@@ -17,3 +17,15 @@ import mock
 
 # Mock out secrets to make py35 happy.
 sys.modules['secrets'] = mock.MagicMock()
+
+# Patch out lsb_release() and get_platform() as unit tests should be fully
+# insulated from the underlying platform.  Unit tests assume that the system is
+# ubuntu jammy.
+mock.patch(
+    'charmhelpers.osplatform.get_platform', return_value='ubuntu'
+).start()
+mock.patch(
+    'charmhelpers.core.host.lsb_release',
+    return_value={
+        'DISTRIB_CODENAME': 'jammy'
+    }).start()
